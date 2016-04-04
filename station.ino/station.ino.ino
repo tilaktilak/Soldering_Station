2c6b27ec4635910ba4d0f9befa83f2a0c42e4eff
tilaktilak <pierre.tilak@gmail.com>
2016-04-04 22:02:16 +0200

    Bouton et LCD OK

--- station.ino/station.ino.ino
+++ station.ino/station.ino.ino
@@ -1,5 +1,5 @@
 /*
-  The circuit:
+   The circuit:
  * LCD RS pin to digital pin 12
  * LCD Enable pin to digital pin 11
  * LCD D4 pin to digital pin 5
@@ -15,9 +15,9 @@
  */
 
 #define temp A0
-#define A A1
-#define B A2
-#define C A3
+#define CLK 8
+#define DT 7
+#define SW 6
 
 #define MOS 6
 // include the library code:
@@ -25,78 +25,102 @@
 #include <Encoder.h>
 
 // initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(12, 11, 2, 3, 4, 5);
 
-Encoder myEnc(A1, A2);
+Encoder myEnc(CLK, DT);
 
 
 void setup() {
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("Starting up ...");
-  pinMode(temp,INPUT);
-  pinMode(A,INPUT);
-  pinMode(B,INPUT);
-  pinMode(C,INPUT);
+    // set up the LCD's number of columns and rows:
+    lcd.begin(16, 2);
+    // Print a message to the LCD.
+    lcd.print("Starting up ...");
+    pinMode(temp,INPUT);
+    pinMode(CLK,INPUT);
+    pinMode(DT,INPUT);
+    pinMode(SW,INPUT);
 
-  pinMode(MOS,OUTPUT);
+    // Initiate Pull-up on switch input
+    digitalWrite(SW,HIGH);
+
+    pinMode(MOS,OUTPUT);
 }
 
-void loop() {
-  float consigne = 280.0;
-  
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print("Temperature :");
-  static long oldPosition = -999;
- 
-  if(digitalRead(C)==1){ // If we press button
-    while(digitalRead(C)==1); // Anti-bounce
-    while(digitalRead(C)!=1){
-      lcd.setCursor(0, 1);
-      lcd.print("Set New Value :");
-      long newPosition = myEnc.read();
-      if (newPosition != oldPosition) {
-        oldPosition = newPosition;
-        Consigne += 0.01 * newPosition; // Set new consigne
-      }
-      lcd.print(Consigne);
-    }
-    while(digitalRead(C)==1); // Anti-bounce
-  }
-
-    static float old_error = 0;
-    static old_time = 0;
-  
-    float new_time = millis();
-    float dt = new_time - old_time;
-    
-    float new_error = get_consigne()- get_temp(); // Process new error
-    
-    float derivative = (new_error-old_error)/dt;
-    float integral += new_error*dt;
-  
-    old_error = new_error;
-  
-    float KP = 10;
-    float KD = 1;
-    float KI = 0.1;
-    float command = KP*error + KD*derivative + KI*integral;
-    set_temp(command); 
-  
-}
+float old_error,old_time,new_time,dt,new_error,derivative,integral,KP,KI,KD,command;
 
 float get_temp(){
-  int temp = analogRead(temp);
-  return 3.14*temp;  
+    int temp = analogRead(temp);
+    return ((float)temp * (5.0f / 1023.0f));
 }
 
-set_temp(float temp){
-  int command = temp * TEMP_MAX / 255;
-  analogWrite(MOS,command);
+#define DEFAULT_TEMP 280
+
+uint16_t Consigne = DEFAULT_TEMP;
+uint16_t last_display_update_ms = 0;
+
+
+/**
+ * @brief MAIN LOOP
+ *
+ */
+void loop() {
+    long newPosition;
+
+    // Pull-up do not stay setted
+    digitalWrite(SW,HIGH);
+
+    // Initialize encoder value to default
+    myEnc.write(DEFAULT_TEMP<<2);
+
+    // Setting Mode loop
+    if(digitalRead(SW)==0){ // If we press button
+        while(digitalRead(SW)==0); // Anti-bounce
+        lcd.setCursor(0,0);
+        lcd.print("Set value : ");
+        while(digitalRead(SW)==1){
+            newPosition = myEnc.read();
+            Consigne = newPosition>>2; // Set new consigne
+            lcd.setCursor(11,0);
+            lcd.print(Consigne);
+        }
+        while(digitalRead(SW)==0); // Anti-bounce
+    }
+
+    new_time = millis();
+    dt = new_time - old_time;
+
+    // Update display content
+    if(last_display_update_ms + 500 <= new_time){
+        lcd.setCursor(0, 0);
+        lcd.print("Temp : ");
+        lcd.print(Consigne);
+        lcd.print(" C     ");
+        lcd.setCursor(0,1);
+        lcd.print(get_temp());
+        old_error = 0;
+        old_time = 0;
+        last_display_update_ms = new_time;
+    }
+
+    new_error = Consigne - get_temp(); // Process new error
+
+    derivative = (new_error-old_error)/dt;
+    integral += new_error*dt;
+
+    old_error = new_error;
+
+    KP = 10;
+    KD = 1;
+    KI = 0.1;
+    command = KP*new_error + KD*derivative + KI*integral;
+    set_temp(command);
+
+}
+
+#define TEMP_MAX 480
+void set_temp(float temp){
+    int command = temp * TEMP_MAX / 255;
+    analogWrite(MOS,command);
 }
 
 
